43922c732ce5a0c5bd6b810308c79ddd89f27438
Ehijator Amagbewan <ehijator.amagbewan@outlook.com>
2024-04-23 00:25:44 +0100

    Deployment of lamp stack

--- project.sh
+++ project.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+
 #update your linux system
-sudo apt update
+sudo apt update -y
 #install your apache webserver
-sudo apt install apache2
+sudo apt install apache2 -y
 #add the php ondrej repository
 sudo add-apt-repository ppa:ondrej/php
 #update your repository again
-sudo apt update
+sudo apt update -y
 # install php8.2
-sudo apt install php8.2
+sudo apt install php8.2 -y
 #install some of those php dependencies that are needed for laravel to work
-sudo apt install php8.2-curl php8.2-dom php8.2-mbstring php8.2-xml php8.2-mysql zip unzip
+sudo apt install php8.2-curl php8.2-dom php8.2-mbstring php8.2-xml php8.2-mysql zip unzip -y
 #enable rewrite
 sudo a2enmod rewrite
 #restart your apache server
@@ -27,8 +28,8 @@ sudo git clone https://github.com/laravel/laravel.git
 sudo chown -R root laravel
 cd laravel/
 #install composer autoloader
-composer install --optimize-autoloader --no-dev
-composer update
+composer install --optimize-autoloader --no-dev --no-interaction
+composer update --no-interaction
 #copy the content of the default env file to .env 
 sudo cp .env.example .env
 sudo chown -R www-data storage
@@ -49,22 +50,21 @@ sudo echo '<VirtualHost *:80>
 sudo a2ensite latest.conf
 sudo a2dissite 000-default.conf
 sudo systemctl restart apache2
-sudo apt install mysql-server
-sudo apt install mysql-client
+sudo apt install mysql-server -y
+sudo apt install mysql-client -y
 sudo systemctl start mysql
-sudo mysql -uroot -e "CREATE DATABASE Tinubu;"
-sudo mysql -uroot -e "CREATE USER 'shettima'@'localhost' IDENTIFIED BY 'cardoso';"
-sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON Tinubu.* TO 'shettima'@'localhost';"
+sudo mysql -uroot -e "CREATE DATABASE Ehijator;"
+sudo mysql -uroot -e "CREATE USER 'Amagbewan'@'localhost' IDENTIFIED BY 'felix';"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON Ehijator.* TO 'Amagbewan'@'localhost';"
 cd /var/www/laravel
 sudo sed -i 's/DB_CONNECTION=mysql/DB_CONNECTION=mysql/' .env
 sudo sed -i 's/DB_HOST=localhost/DB_HOST=localhost/' .env
 sudo sed -i 's/DB_PORT=3306/DB_PORT=3306/' .env
-sudo sed -i 's/DB_DATABASE=Tinubu/DB_DATABASE=Tinubu/' .env
-sudo sed -i 's/DB_USERNAME=shettima/DB_USERNAME=shettima/' .env
-sudo sed -i 's/DB_PASSWORD=cardoso/DB_PASSWORD=cardoso/' .env
+sudo sed -i 's/DB_DATABASE=Tinubu/DB_DATABASE=Ehijator/' .env
+sudo sed -i 's/DB_USERNAME=shettima/DB_USERNAME=Amagbewan/' .env
+sudo sed -i 's/DB_PASSWORD=cardoso/DB_PASSWORD=felix/' .env
 sudo php artisan key:generate
 sudo php artisan storage:link
 sudo php artisan migrate
 sudo php artisan db:seed
 sudo systemctl restart apache2
-
